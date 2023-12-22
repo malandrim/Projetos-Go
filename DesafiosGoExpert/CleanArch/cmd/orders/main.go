8d0528ed3ec2052e58ce95e93a887ef472268959
malandrim <lh.malandrim@gmail.com>
2023-12-21 22:31:27 -0300

    adding grpc service

--- DesafiosGoExpert/CleanArch/cmd/orders/main.go
+++ DesafiosGoExpert/CleanArch/cmd/orders/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"net"
 
 	"github.com/malandrim/Projetos-Go/DesafiosGoExpert/CleanArch/configs"
-	"github.com/malandrim/Projetos-Go/DesafiosGoExpert/CleanArch/internal/infra/web/webserver"
+	service "github.com/malandrim/Projetos-Go/DesafiosGoExpert/CleanArch/internal/infra/grpc"
+	"github.com/malandrim/Projetos-Go/DesafiosGoExpert/CleanArch/internal/infra/grpc/pb"
 	"github.com/malandrim/Projetos-Go/DesafiosGoExpert/CleanArch/pkg/events"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -23,12 +27,52 @@ func main() {
 	}
 	defer db.Close()
 
-	eventDispatcher := events.NewEventDispatcher()
+	//	rabbitMQChannel := getRabbitMQChannel()
 
-	webserver := webserver.NewWebServer(configs.WebServerPort)
+	eventDispatcher := events.NewEventDispatcher()
+	//eventDispatcher.Register("OrderCreated", &handler.OrderCreatedHandler{
+	//		RabbitMQChannel: rabbitMQChannel,
+	//	})
+
+	createOrderUseCase := NewCreateOrderUseCase(db, eventDispatcher)
+
+	/* webserver := webserver.NewWebServer(configs.WebServerPort)
 	webOrderHandler := NewWebOrderHandler(db, eventDispatcher)
 	webserver.AddHandler("/order", webOrderHandler.Create)
 	fmt.Println("Starting web server on port", configs.WebServerPort)
 	go webserver.Start()
+	*/
+	grpcServer := grpc.NewServer()
+	createOrderService := service.NewOrderService(*createOrderUseCase)
+	pb.RegisterOrderServiceServer(grpcServer, createOrderService)
+	reflection.Register(grpcServer)
+
+	fmt.Println("Starting gRPC server on port", configs.GRPCServerPort)
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", configs.GRPCServerPort))
+	if err != nil {
+		panic(err)
+	}
+	grpcServer.Serve(lis)
+
+	//	srv := graphql_handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+	//		CreateOrderUseCase: *createOrderUseCase,
+	//	}}))
+	//	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	//	http.Handle("/query", srv)
+
+	//	fmt.Println("Starting GraphQL server on port", configs.GraphQLServerPort)
+	//	http.ListenAndServe(":"+configs.GraphQLServerPort, nil)
 
 }
+
+//func getRabbitMQChannel() *amqp.Channel {
+//	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/") // alterar para variavel de ambiente
+//	if err != nil {
+//		panic(err)
+//	}
+//	ch, err := conn.Channel()
+//	if err != nil {
+//		panic(err)
+//	}
+//	return ch
+//}
--- DesafiosGoExpert/CleanArch/go.mod
+++ DesafiosGoExpert/CleanArch/go.mod
@@ -9,6 +9,14 @@ require (
 	github.com/spf13/viper v1.18.2
 	github.com/streadway/amqp v1.1.0
 	github.com/stretchr/testify v1.8.4
+	google.golang.org/grpc v1.59.0
+	google.golang.org/protobuf v1.31.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231120223509-83a465c0220f // indirect
 )
 
 require (
