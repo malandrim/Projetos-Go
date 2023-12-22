47a21c26935f21724810914ceac71f4948ed5752
malandrim <lh.malandrim@gmail.com>
2023-12-21 21:38:33 -0300

    organizing folders

---
+++ DesafiosGoExpert/CleanArch/internal/infra/database/order_repository.go
@@ -0,0 +1,39 @@
+package database
+
+import (
+	"database/sql"
+
+	"github.com/malandrim/Projetos-Go/DesafiosGoExpert/CleanArch/internal/entity"
+)
+
+type OrderRepository struct {
+	Db *sql.DB
+}
+
+func NewOrderRepository(db *sql.DB) *OrderRepository {
+	return &OrderRepository{Db: db}
+}
+
+func (r *OrderRepository) Save(order *entity.Order) error {
+	stmt, err := r.Db.Prepare("INSERT INTO orders (id, price, tax, final_price) VALUES (? , ?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(order.ID, order.Price, order.Tax, order.FinalPrice)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *OrderRepository) GetOrderByID(order *entity.Order) error {
+	stmt, err := r.Db.Prepare("SELECT id, price, tax, final_price FROM orders WHERE id = ? ")
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(order.ID, order.Price, order.Tax, order.FinalPrice)
+	if err != nil {
+		return err
+	}
+	return nil
+}
